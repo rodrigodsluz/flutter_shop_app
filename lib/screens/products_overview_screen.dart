bee192c192c75dec706a98941caccabf882030af
Rodrigo Luz <rodrigodsluz@gmail.com>
2022-11-04 17:23:41 -0300

    feat: using a side drawer

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -8,6 +8,8 @@ import '../screens/cart_screen.dart';
 
 import '../providers/cart.dart';
 
+import '../widgets/app_drawer.dart';
+
 enum FilterOptions {
   Favorites,
   All,
@@ -64,6 +66,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
               )),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
