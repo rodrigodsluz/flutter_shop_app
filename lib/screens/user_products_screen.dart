2324e1b9fceae3864a4b66f9db7aff535f665109
Rodrigo Luz <rodrigodsluz@gmail.com>
2022-11-07 11:42:54 -0300

    feat: adding input fields

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -5,6 +5,7 @@ import '../providers/products.dart';
 import '../widgets/app_drawer.dart';
 
 import '../widgets/user_product_item.dart';
+import '../screens/edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -19,7 +20,9 @@ class UserProductsScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           ),
         ],
       ),
