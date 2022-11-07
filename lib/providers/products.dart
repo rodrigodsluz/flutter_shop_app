e144b4fbb3062509db4f1362706b5e2c507614a6
Rodrigo Luz <rodrigodsluz@gmail.com>
2022-11-07 14:40:53 -0300

    feat: allowing users to delect users

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -88,4 +88,9 @@ class Products with ChangeNotifier {
       print('...');
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((prod) => prod.id == id);
+    notifyListeners();
+  }
 }
--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 import '../screens/edit_product_screen.dart';
 
@@ -34,7 +37,9 @@ class UserProductItem extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
             ),
           ],
         ),
