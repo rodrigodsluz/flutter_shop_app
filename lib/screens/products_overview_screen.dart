843c06d1c70ed407288a357a6b5b5d36209a8227
Rodrigo Luz <rodrigodsluz@gmail.com>
2022-11-08 10:07:25 -0300

    feat: implementing pull to refresh

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -23,15 +23,44 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
+  //var _isInit = true;
+  var _isLoading = false;
 
   @override
   void initState() {
     //Provider.of<Products>(context).fetchAndSetProducts(); // WON'T WORK!
     Future.delayed(Duration.zero).then((_) {
-      Provider.of<Products>(context).fetchAndSetProducts();
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context, listen: false).fetchAndSetProducts().then(
+        (_) {
+          setState(
+            () {
+              _isLoading = false;
+            },
+          );
+        },
+      );
     });
     super.initState();
   }
+
+  // @override
+  // void didChangeDependencies() {
+  //   if (_isInit) {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //     });
+  //   }
+  //   _isInit = false;
+  //   super.didChangeDependencies();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +106,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(_showOnlyFavorites),
     );
   }
 }
